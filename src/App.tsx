e86bf7a8f24f38cb984d6f514c4df52ccc0b4ca3
thennavan-dev <thennavan.dev@gmail.com>
2026-04-03 21:40:37 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/thennavan">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects" element={<Projects />} />
