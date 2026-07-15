ef646fe1283b71a17db82a391a50de9f040cc5a3
thennavan-dev <thennavan.dev.in@gmail.com>
2026-07-15 12:06:42 +0530

    route changed

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/thennavan">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects" element={<Projects />} />
